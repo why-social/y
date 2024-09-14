74d099cfc575210a1c190ef92da81fd29e5fbb11
Razvan Albu <razvana@student.chalmers.se>
2024-09-14 12:37:23 +0200

    #5 Add header auth checker

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,7 +1,40 @@
-module.exports = function(app, model) {
+module.exports = function (app, model) {
+    //GET endpoints region start
     app.get("/api/v1/posts/", function (req, res) {
-        res.json({ 'message': 'Posts' })
+        res.json({ 'message': 'GET test' })
     });
+    //GET endpoints region end
+
+    //POST endpoints region start
+    app.post("/api/v1/posts/", function (req, res) {
+        if (checkAuthHeader(req, res)) {
+            res.json({ 'message': 'POST test' })
+        }
+    });
+    //POST endpoints region end
+
+    //PATCH endpoints region start
+    app.patch("/api/v1/posts/:id", function (req, res) {
+        if (checkAuthHeader(req, res)) {
+            res.json({ 'message': 'PATCH test' })
+        }
+    });
+    //PATCH endpoints region end
+
+    //DELETE endpoints region start
+
+    //DELETE endpoints region end
+
+    function checkAuthHeader(req, res) {
+        //TODO authentication header verification
+        if (req.header("Auth") == undefined || false /*replace with actual header check*/) {
+            res.status(401).json({ "message": "Unauthorised" });
+
+            return false;
+        }
+
+        return true;
+    }
 
     console.log("ROUTE: Posts routes registered.")
 };
