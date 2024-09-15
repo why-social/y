5887706ebd070810af63c38ddc78aaaec3e8ea72
Aliaksei Khval <guskhval@student.gu.se>
2024-09-15 22:00:37 +0200

    #4 Add authentication support and password hashing

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,31 +1,55 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("../db/createDB").mongoose;
+const authMiddleware = require("../middleware/auth");
 
 //#region GET
-router.get("/user/:id", (req,res) => {
+router.get("/api/user/:id", authMiddleware, async (req, res) => {
+	try {
+		// Get user by id from db
+		let result = await mongoose.models["Users"].findById(req.params.id).exec();
 
-	//TODO: Replace with actual user page
+		// If user not found return 404
+		if(!result) return res.status(404).json({message: "User not found"});
 
-	res.send(`<html><body>User id: ${req.params.id}</body></html>`);
-});
+		let partialResponse = {
+			name: result.name,
+			email: result.email,
+			about_me: result.about_me,
+			username: result.username,
+			join_date: result.join_date,
+			birthday: result.birthday,
+			last_time_posted: result.last_time_posted,
+			profile_picture: result.profile_picture,
+		}
 
-router.get("/api/user/:id", async (req,res) => {
+		// If user is not authenticated, do not return email
+		if(!req.isAuth) delete partialResponse.email;
 
-	//TODO: Add special case for authenticated users
-
-	let result = await mongoose.models["Users"].findById(req.params.id).exec();
-	res.json(result);
+		res.json(partialResponse);
+	} catch (err) {
+		res.status(500).json({message: "Server error"});
+	}
 });
 
 router.get("/api/user/:id/followers", async (req,res) => {
-	let result = await mongoose.models["User_follows_user"].find({follows: req.params.id}).exec();
-	res.json(result);
+	try{
+		let result = await mongoose.models["User_follows_user"].find({follows: req.params.id}).exec();
+		if(!result) return res.status(404).json({message: "User not found"});
+		res.json(result);
+	} catch (err) {
+		res.status(500).json({message: "Server error"});
+	}
 });
 
 router.get("/api/user/:id/following", async (req,res) => {
-	let result = await mongoose.models["User_follows_user"].find({follower: req.params.id}).exec();
-	res.json(result);
+	try{
+		let result = await mongoose.models["User_follows_user"].find({follower: req.params.id}).exec();
+		if(!result) return res.status(404).json({message: "User not found"});
+		res.json(result);
+	} catch (err) {
+		res.status(500).json({message: "Server error"});
+	}
 });
 //#endregion
 
