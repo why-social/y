f8a09fe2397a5f9f7cda77d176fe3adb524595d1
Razvan Albu <razvana@student.chalmers.se>
2024-09-17 10:47:57 +0200

    #6 Refactor error handling

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -7,24 +7,29 @@ const authMiddleware = require("../middleware/auth");
 //#region GET
 router.get("/api/v1/comments/:id",
     async function (req, res) {
-        let obj = await getCommentById(req.params.id);
+        try {
+            let comment = await getCommentById(req.params.id);
 
-        return res.status(obj["status"])
-            .json(obj["body"]);
+            return res.status(200)
+                .json(comment);
+        } catch (error) {
+            return res.status(404)
+                .json({ message: error.message });
+        }
     });
 
 router.get("/api/v1/comments/:id/images/",
     async function (req, res) {
-        let obj = await getCommentById(req.params.id);
+        try {
+            let comment = await getCommentById(req.params.id);
 
-        if (obj["status"] != 200) {
-            return res.status(obj["status"])
-                .json(obj["body"]);
-        } else {
             return res.status(200)
                 .json({
-                    images: obj["body"]["images"]
+                    images: comment["images"]
                 });
+        } catch (error) {
+            return res.status(404)
+                .json({ message: error.message });
         }
     });
 
@@ -34,7 +39,7 @@ router.get("/api/v1/comments/user/:id",
             let result = await models["Comments"]
                 .find({ user: req.params.id }).exec();
 
-            if (!result) {
+            if (!(result && result.length)) {
                 return res.status(404)
                     .json({ message: "Not found." });
             }
@@ -56,17 +61,17 @@ router.get("/api/v1/comments/user/:id",
 
 router.get("/api/v1/comment/:comment_id/likes/:user_id",
     async function (req, res) {
-        let obj = await getCommentById(req.params.comment_id);
+        try {
+            let comment = await getCommentById(req.params.comment_id);
 
-        if (obj["status"] != 200) {
-            return res.status(obj["status"])
-                .json(obj["body"]);
-        } else {
             return res.status(200)
                 .json({
-                    likes: obj["body"]["likes"]
+                    likes: comment["likes"]
                         .includes(req.params.user_id)
                 });
+        } catch (error) {
+            return res.status(404)
+                .json({ message: error.message });
         }
     });
 //#endregion
@@ -74,24 +79,25 @@ router.get("/api/v1/comment/:comment_id/likes/:user_id",
 //#region POST
 router.post("/api/v1/comments/",
     authMiddleware, async function (req, res) {
-        res = validateBody(req, res);
-
-        if (res.statusCode == 404) {
-            return res;
-        }
-
-        let comment = new models["Comments"](req.body);
-
         try {
-            await comment.save();
+            if (validateBody(req, res)) {
+                let comment = new models["Comments"](req.body);
 
-            //TODO also update images table
+                try {
+                    await comment.save();
 
-            return res.status(200)
-                .json({ id: comment["_id"] });
+                    //TODO also update images table
+
+                    return res.status(200)
+                        .json({ id: comment["_id"] });
+                } catch (error) {
+                    return res.status(404)
+                        .json({ message: "Malformed request body." })
+                }
+            }
         } catch (error) {
             return res.status(404)
-                .json({ message: "Malformed request body." })
+                .json({ message: error.message });
         }
     });
 //#endregion
@@ -100,31 +106,25 @@ router.post("/api/v1/comments/",
 router.patch("/api/v1/comments/:id",
     authMiddleware, async function (req, res) {
         try {
-            res = validateBody(req, res);
+            if (validateBody(req, res)) {
+                let target = await models["Comments"]
+                    .findOneAndUpdate({ _id: req.params.id },
+                        {
+                            content: req.body["content"],
+                            images: req.body["images"],
+                            is_edited: true
+                        }
+                    );
 
-            if (res.statusCode == 404) {
-                return res;
-            }
+                if (target) {
+                    //TODO also update images table
 
-            let target = await models["Comments"]
-                .findOneAndUpdate({ _id: req.params.id },
-                    {
-                        content: req.body["content"],
-                        images: req.body["images"],
-                        is_edited: true
-                    }
-                );
-
-            console.log(target);
-
-            if (target) {
-                //TODO also update images table
-
-                return res.status(200)
-                    .json({ message: "Successfully updated." });
-            } else {
-                return res.status(404)
-                    .json({ message: "Not found." });
+                    return res.status(200)
+                        .json({ message: "Successfully updated." });
+                } else {
+                    return res.status(404)
+                        .json({ message: "Not found." });
+                }
             }
         } catch (error) {
             res.status(404);
@@ -135,7 +135,8 @@ router.patch("/api/v1/comments/:id",
                 res.json({ message: "Could not update." });
             }
 
-            return res;
+            return res.status(404)
+                .json({ message: error.message });
         }
     });
 //#endregion
@@ -161,9 +162,9 @@ router.delete("/api/v1/comments/:id",
             res.status(404);
 
             if (error.name === 'CastError') {
-                res.json({ message: "Malformed user identifier." });
+                res.json({ message: "Malformed user identifier or request body." });
             } else {
-                res.json({ message: "Could not remove." });
+                res.json({ message: error.message });
             }
 
             return res;
@@ -173,47 +174,37 @@ router.delete("/api/v1/comments/:id",
 
 //#region Utility
 async function getCommentById(id) {
+    let result;
+
     try {
-        let result = await models["Comments"]
+        result = await models["Comments"]
             .findById(id).exec();
-
-        if (!result) {
-            return {
-                status: 404,
-                body: { message: "Not found." }
-            }
-        }
-
-        return {
-            status: 200,
-            body: result
-        };
     } catch (error) {
-        let res = {
-            status: 404
-        };
-
         if (error.name === 'CastError') {
-            res["body"] = { message: "Malformed comment identifier." };
+            throw new Error("Malformed comment identifier.");
         } else {
-            res["body"] = { message: "Malformed request." };
+            throw new Error("Malformed request.");
         }
-
-        return res;
     }
+
+    if (!result) {
+        throw new Error("Not found.");
+    }
+
+    return result;
 }
 
-function validateBody(req, res) {
+function validateBody(req) {
     if (!(req.body["content"] ||
         (req.body["images"] &&
             req.body["images"].length))) {
-        return res.status(404)
-            .json({ message: "Comment needs to have at least a content string or an image." });
+
+        throw new Error("Comment needs to have at least a content string or an image.");
     }
 
     //TODO validate content and images
 
-    return res;
+    return true;
 }
 //#endregion
 
