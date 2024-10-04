794e47e21353a59183843ef3ee72238c25418f7c
Maksims Orlovs <orlovs@student.chalmers.se>
2024-10-04 02:14:51 +0200

    #32 Add recents tab

--- client/src/views/homeTabs/Recent.vue
+++ client/src/views/homeTabs/Recent.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
-    <p>Placeholder</p>
+    <Post v-for="post in posts" :post="post" :key="post._id" />
   </div>
 </template>
 
 <script>
-
+import { Api } from '@/Api'
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    Api.get('/v1/posts', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    })
+      .then((response) => {
+        this.posts = response.data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+}
 </script>
 
 <style scoped>
