5359fe961dff24bc0a3329154d103b867b97e649
Aliaksei Khval <guskhval@student.gu.se>
2024-10-15 15:43:10 +0200

    #47 Fix the bug

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -12,7 +12,7 @@ const router = express.Router();
 //#region GET
 router.get("/api/v1/posts", async function (req, res, next) {
 	try {
-		const posts = await models.Posts.find()
+		const posts = await models.Posts.find({ is_deleted: false})
 			.populate({
 				path: 'author', select: '_id name username profile_picture'
 			})
--- server/routes/users.js
+++ server/routes/users.js
@@ -135,7 +135,7 @@ router.get("/api/v1/users/:id/followings", async (req, res, next) => {
 // Returns all posts authored by the user with id :id
 router.get("/api/v1/users/:id/posts", async function (req, res, next) {
 	try {
-		const posts = await mongoose.models["Posts"].find({ author: req.params.id }).populate({
+		const posts = await mongoose.models["Posts"].find({ author: req.params.id, is_deleted: false }).populate({
 			path: 'author', select: '_id name username profile_picture',
 		}).lean().exec();
 
@@ -167,7 +167,7 @@ router.get("/api/v1/users/:id/comments", async function (req, res, next) {
 		if(!userExists) throw new NotFoundError(errorMsg.USER_NOT_FOUND);
 		
 		let result = await mongoose.models["Comments"]
-			.find({ author: req.params.id })
+			.find({ author: req.params.id, is_deleted: false })
 			.populate({
 				path: 'author', select: '_id name username profile_picture',
 			})
